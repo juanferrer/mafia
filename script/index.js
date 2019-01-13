0a3ea10e6f06f9c22752c899d6f8be2fa7c29623
juanferrer <juanemilioferrer@outlook.com>
2019-01-13 22:45:46 +0000

    Start working on PHP logic

---
+++ script/index.js
@@ -0,0 +1,3 @@
+/* globals */
+
+// Server sent events: https://www.w3schools.com/html/html5_serversentevents.asp
---
+++ script/mafia.php
@@ -0,0 +1,27 @@
+<?php
+
+// username: crazy_maniac
+// password: %h$4Cb1LlXhj
+
+// Join the game with the specified ID or create a new one and return a new gameID
+// gameID is a string of 3 alphanumeric caracters
+function joinGame($gameID, $player)
+{
+    // If $gameID empty, create new game
+
+    // otherwise, join game with info
+}
+
+//
+function leaveGame($gameID, $player)
+{
+    // If $gameID empty, ignore
+
+    // otherwise, leave game
+}
+
+// Update
+function updateGameState($gameID)
+{
+
+}
